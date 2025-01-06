bc769b2f3f7ef6663e1a74d4d4397868fd556ff1
MariamSkaria <mariamse336@gmail.com>
2025-01-06 23:52:31 +0530

    Update facedet.py

--- facedet.py
+++ facedet.py
@@ -16,13 +16,5 @@ def detfacecamera():
             break
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.imshow('Live Face Detection', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-detfacecamera()
 
    
